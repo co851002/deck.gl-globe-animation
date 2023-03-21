24e8816ea22d4eeb933cae1e05c14aae576a8aa9
chris oosthuizen <co851002@gmail.com>
2023-03-21 13:59:23 +0000

    added spin, easing on select with button

--- src/App.js
+++ src/App.js
@@ -1,93 +1,93 @@
 import './App.css';
-import React, { useState, useRef, useCallback } from 'react';
-import DeckGL from '@deck.gl/react';
-import { BitmapLayer } from '@deck.gl/layers';
-import { _GlobeView, COORDINATE_SYSTEM } from '@deck.gl/core';
-import { TileLayer } from '@deck.gl/geo-layers';
-import { AnimationLoop, Model, Geometry } from '@luma.gl/core';
-import RotatingGlobe from './components/RotatingGlobe2'
+import React from 'react';
+// import DeckGL from '@deck.gl/react';
+// import { BitmapLayer } from '@deck.gl/layers';
+// import { _GlobeView, COORDINATE_SYSTEM } from '@deck.gl/core';
+// import { TileLayer } from '@deck.gl/geo-layers';
+// import { AnimationLoop, Model, Geometry } from '@luma.gl/core';
+import RotatingGlobe from './components/RotatingGlobe3'
 
-const views = new _GlobeView({
-  resolution: 10
-});
+// const views = new _GlobeView({
+//   resolution: 10
+// });
 
-const initialViewState = {
-  longitude: 2.27,
-  latitude: 30.86,
-  zoom: 0,
-  minZoom: 0,
-  maxZoom: 20
-};
+// const initialViewState = {
+//   longitude: 2.27,
+//   latitude: 30.86,
+//   zoom: 0,
+//   minZoom: 0,
+//   maxZoom: 20
+// };
 
-const controller = {
-  controller: true,
-}
+// const controller = {
+//   controller: true,
+// }
 
-const layers = [
-  new TileLayer({
-    data: 'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    minZoom: 0,
-    maxZoom: 19,
-    tileSize: 256,
+// const layers = [
+//   new TileLayer({
+//     data: 'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
+//     minZoom: 0,
+//     maxZoom: 19,
+//     tileSize: 256,
 
-    renderSubLayers: props => {
-      const {
-        bbox: { west, south, east, north }
-      } = props.tile;
+//     renderSubLayers: props => {
+//       const {
+//         bbox: { west, south, east, north }
+//       } = props.tile;
 
-      return new BitmapLayer(props, {
-        data: null,
-        image: props.data,
-        _imageCoordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
-        bounds: [west, south, east, north]
-      });
-    }
-  })
-]
+//       return new BitmapLayer(props, {
+//         data: null,
+//         image: props.data,
+//         _imageCoordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
+//         bounds: [west, south, east, north]
+//       });
+//     }
+//   })
+// ]
 
-const viewState = {
-  latitude: 0,
-  longitude: 0,
-  zoom: 0,
-  minZoom: 0,
-  maxZoom: 20,
-  pitch: 0,
-  bearing: 0,
-};
+// const viewState = {
+//   latitude: 0,
+//   longitude: 0,
+//   zoom: 0,
+//   minZoom: 0,
+//   maxZoom: 20,
+//   pitch: 0,
+//   bearing: 0,
+// };
 
 function App() {
-  const [rotation, setRotation] = useState([0, 0, 0]);
-  const animationLoopRef = useRef(null);
+  // const [rotation, setRotation] = useState([0, 0, 0]);
+  // const animationLoopRef = useRef(null);
 
-  const onClickHandler = useCallback(() => {
-    if (animationLoopRef.current) {
-      animationLoopRef.current.stop();
-      animationLoopRef.current = null;
-    } else {
-      const animationLoop = new AnimationLoop({
-        onInitialize: ({ gl }) => {
-          return {
-            model: new Model(gl, {
-              geometry: new Geometry({
-                drawMode: 'TRIANGLES',
-                vertexCount: 0,
-              }),
-            }),
-          };
-        },
-        onRender: () => {
-          setRotation((prevRotation) => [
-            prevRotation[0] + 1,
-            prevRotation[1] + 1,
-            prevRotation[2] + 1,
-          ]);
-        },
-      });
+  // const onClickHandler = useCallback(() => {
+  //   if (animationLoopRef.current) {
+  //     animationLoopRef.current.stop();
+  //     animationLoopRef.current = null;
+  //   } else {
+  //     const animationLoop = new AnimationLoop({
+  //       onInitialize: ({ gl }) => {
+  //         return {
+  //           model: new Model(gl, {
+  //             geometry: new Geometry({
+  //               drawMode: 'TRIANGLES',
+  //               vertexCount: 0,
+  //             }),
+  //           }),
+  //         };
+  //       },
+  //       onRender: () => {
+  //         setRotation((prevRotation) => [
+  //           prevRotation[0] + 1,
+  //           prevRotation[1] + 1,
+  //           prevRotation[2] + 1,
+  //         ]);
+  //       },
+  //     });
 
-      animationLoopRef.current = animationLoop;
-      animationLoop.start();
-    }
-  }, []);
+  //     animationLoopRef.current = animationLoop;
+  //     animationLoop.start();
+  //   }
+  // }, []);
 
 
 
